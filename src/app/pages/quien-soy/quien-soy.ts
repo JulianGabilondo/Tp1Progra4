a876a24ee1d5cc921ae0bfa8dc1b3e9f185ec8c2
julian <Juliangabilondo@gmail.com>
2026-05-18 23:43:30 -0300

    sprint 1

---
+++ src/app/pages/quien-soy/quien-soy.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-quien-soy',
+  imports: [CommonModule],
+  templateUrl: './quien-soy.html',
+  styleUrl: './quien-soy.css'
+})
+export class QuienSoy {
+
+  usuario:any;
+
+  constructor(private http: HttpClient) {
+
+    this.http
+      .get('https://api.github.com/users/JulianGabilondo')
+      .subscribe(data => {
+
+        this.usuario = data;
+
+      });
+
+  }
+
+}
